b97c95776138cc558f4515272d6e72e019dc64b1
Jonah10001 <95149794+Jonah10001@users.noreply.github.com>
2023-05-03 13:17:17 -0500

    next commit

--- src/components/article.js
+++ src/components/article.js
@@ -10,7 +10,7 @@ export default function Article({text, time, type}) {
             <h2>{type} Text</h2>
 
             { text ? 
-                <p><Changes reason={text}/>{text}</p>
+                <p><Changes reason={text} ref_num={3} text={text}/>{text}</p>
                 :
                 <div>
                     <p>Loading {type.toLowerCase()} text</p>
--- src/components/changes.js
+++ src/components/changes.js
@@ -1,16 +1,37 @@
 import { useState, useEffect, useRef } from 'react';
 import './changes.css'
 
-export default function Changes({reason, color}) {
+const highlightColors = {
+    1: ['#FF4136', '#FF1300'],
+    2: ['#FFA500', '#FF7400'],
+    3: ['#FFDC00', '#D4AF37'],
+    4: ['#2ECC40', '#007E23'],
+    5: ['#0074D9', '#004776'],
+    6: ['#B10DC9', '#7A007A'],
+    7: ['#FF1493', '#C71585'],
+    8: ['#39CCCC', '#008B8B'],
+    9: ['#AAAAAA', '#696969'],
+    10: ['#111111', '#333333'],
+  };
+  
+
+export default function Changes({reason, ref_num, text}) {
     const [isHovered, setIsHovered] = useState(false);
     const boxRef = useRef(null);
+    const highlightRef = useRef(null);
+
+    useEffect(() => {
+        highlightRef.current.style.backgroundColor = highlightColors[ref_num][0];
+    }, []);
 
     useEffect(() => {
         if (isHovered) {
-          boxRef.current.style.display = 'block';
-          boxRef.current.style.top = boxRef.current.clientHeight * -1.2 +`px`;
+        boxRef.current.style.display = 'block';
+        boxRef.current.style.top = boxRef.current.clientHeight * -1.25 +`px`;
+        highlightRef.current.style.backgroundColor = highlightColors[ref_num][0];
         } else {
-            boxRef.current.style.display = 'none';
+        boxRef.current.style.display = 'none';
+        highlightRef.current.style.backgroundColor = highlightColors[ref_num][1];
         }
     }, [isHovered]);
 
@@ -23,12 +44,10 @@ export default function Changes({reason, color}) {
     };
 
     return (
-        <span className="highlight" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <span className="box sb3" ref={boxRef}>Reason for change:
-             Reason for change: Reason for change: {reason}</span>
-            “Let’s be frank, anyone who associated with a gang at some
-             point in their life, they’ve done something, whether they
-              have been arrested or not,” he said.
+        <span className={`highlight`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+        ref={highlightRef}>
+            <span className="box sb3" ref={boxRef}>Reason for change: {reason}</span>
+            {text}
         </span>
     )
 }
