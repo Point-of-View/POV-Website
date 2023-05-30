1882215114080502f5d51bf5227a7ab2bc998430
Jcheck27 <jordan.checkoff@gmail.com>
2023-05-30 15:36:12 -0500

    new colors

--- src/components/changes.js
+++ src/components/changes.js
@@ -4,29 +4,22 @@ import { Tooltip } from 'react-tooltip'
 
 export default function Changes({reason, index, text, isHovered, setIsHovered, classValue}) {
 
-    const highlightColors = {
-        1: ['#FF9E9E', '#FFB3B3'],
-        2: ['#FFA74D', '#FFc080'],
-        3: ['#FFF74D', '#FFF080'],
-        4: ['#6EF74D', '#6EF080'],
-        5: ['#52F74D', '#52F080'],
-        6: ['#52874D', '#528080'],
-        7: ['#96574D', '#965080'],
-        8: ['#FF574D', '#FF5080'],
-        9: ['#FF9E9E', '#FFB3B3'],
-        10: ['#FFA74D', '#FFc080'],
-        11: ['#FFF74D', '#FFF080'],
-        12: ['#6EF74D', '#6EF080'],
-        13: ['#52F74D', '#52F080'],
-        14: ['#52874D', '#528080'],
-        15: ['#96574D', '#965080']
-      };
+    const highlightColors = [
+        ['#FF9E9E', '#FFB3B3'],
+        ['#FFA74D', '#FFc080'],
+        ['#FFF74D', '#FFF080'],
+        ['#6EF74D', '#6EF080'],
+        ['#52F74D', '#52F080'],
+        ['#52874D', '#528080'],
+        ['#96574D', '#965080'],
+        ['#FF574D', '#FF5080'],
+    ];
     
     return (                    
         <span>
             <Tooltip anchorSelect={"."+ classValue} place="top" className='box'>{reason}</Tooltip>
             <p className={classValue}
-            style={{ backgroundColor: ((isHovered === index) ? highlightColors[index + 1][0] : highlightColors[index + 1][1]), display: 'inline' }}
+            style={{ backgroundColor: ((isHovered === index) ? highlightColors[index % 7][0] : highlightColors[index % 7][1]), display: 'inline' }}
             onMouseEnter={() => setIsHovered(index)} onMouseLeave={() => setIsHovered(-1)}>
             {text}
             </p>
